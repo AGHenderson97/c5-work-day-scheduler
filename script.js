25b87ed1de5b0b6946ebf7da322cb1637b1a0f06
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-19 00:42:21 -0400

    days.js, variables, time blocks

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ $(document).ready(function() {
   $("button").on("click", saveData);
 })
 
+function getCurrentDay() {
+  let currentDay = days.formatDate(new Date(), "dddd, MMMM Do");
+  $("#currentDay").text(currentDay);
+  }
+
+  function createTimeBlocks() {
+    let container = $(".container");
+    let startOfDay = days.startOf(new Date(), "day");
+    let myTime = days.formatDate(days.add(startOfDay, 9, "hour"), "h:mma");
+    for(let i = 0; i < 9; i++){
+
+  let row = $("<div>");
+  let hourSpan = $("<span>");
+  let textSpan = $("<span>");
+  let textArea = $("<textarea>");
+  let button = $("<button>");
+  let icon = $("<i>");
